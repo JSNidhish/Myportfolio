23c2ea0f7e57ddd9b149f89761101e8504ea29ba
J S Nidhish <nidhishshetty018@gmail.com>
2026-03-10 01:51:50 +0530

    Initial portfolio upload

--- README.md
+++
@@ -1,2 +0,0 @@
-# Myportfolio
-My first project 
--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-  document.querySelector(".nav-links").classList.toggle("active");
-}
-
-document.getElementById("contact-form").addEventListener("submit", function(e){
-  e.preventDefault();
-  alert("Message Sent Successfully!");
-});
